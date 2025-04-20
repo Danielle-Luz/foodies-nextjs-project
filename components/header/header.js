56a21b512a20c5363beea4c336069edded7c46c7
Danielle Luz <daniln2209@gmail.com>
2025-04-20 16:23:54 -0300

    feat: styled navbar links

--- components/header/header.js
+++ components/header/header.js
@@ -9,12 +9,12 @@ export default function Header() {
         <img src={logo.src} alt="Ilustration of different types of meals over a plate" />
         <h1>NextLevel Food</h1>
       </Link>
-      <ul>
+      <ul className={styles.navbar}>
         <li>
-          <Link href="/meals">Browse meals</Link>
+          <Link className={styles.navbarLink} href="/meals">Browse meals</Link>
         </li>
         <li>
-          <Link href="/community">Foodies Community</Link>
+          <Link className={styles.navbarLink} href="/community">Foodies Community</Link>
         </li>
       </ul>
     </header>
