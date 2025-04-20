d2e2693eff05fef0f7e5d43202529fcb9d81440d
Danielle Luz <daniln2209@gmail.com>
2025-04-20 17:29:26 -0300

    feat: replace img tag for image component

--- components/header/header.js
+++ components/header/header.js
@@ -1,12 +1,13 @@
 import logo from "@/assets/logo.png";
 import Link from "next/link";
 import styles from "./styles.module.css";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <Link className={styles.logo} href="./">
-        <img src={logo.src} alt="Ilustration of different types of meals over a plate" />
+        <Image src={logo} alt="Ilustration of different types of meals over a plate" priority />
         <h1>NextLevel Food</h1>
       </Link>
       <ul className={styles.navbar}>
