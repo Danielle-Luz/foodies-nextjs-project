57ec9f8e659535a5770a716575030b0b3f7bcfcf
Danielle Luz <daniln2209@gmail.com>
2025-04-21 05:10:56 -0300

    feat: add imports

--- app/community/page.js
+++ app/community/page.js
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+import mealIcon from '@/assets/icons/meal.png';
+import communityIcon from '@/assets/icons/community.png';
+import eventsIcon from '@/assets/icons/events.png';
+
 export default function Community() {
   return (
     <main>
