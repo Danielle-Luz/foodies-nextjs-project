dfb45810aa711f36dd5748b0cda468ece1f79f1a
Danielle Luz <daniln2209@gmail.com>
2025-04-20 22:31:21 -0300

    feat: add slider animation

--- components/image-slider/image-slider.js
+++ components/image-slider/image-slider.js
@@ -1,24 +1,29 @@
 'use client'
 
 import Image from "next/image";
+import styles from "./styles.module.css";
 import { images } from "./data";
 import { useState } from "react";
-import styles from "./styles.module.css";
 
 export default function ImageSlider() {
   const [visibleImage, setVisibleImage] = useState({index: 0, data: images[0]});
 
-  setInterval(() => {
+  function changeVisibleImage() {
     const nextIndex = visibleImage.index + 1;
-
+  
     if(nextIndex < images.length) {
       setVisibleImage({index: nextIndex, data: images[nextIndex]});
     } else {
       setVisibleImage({index: 0, data: images[0]});
     }
-  }, 5000);
+  }
 
   return (
-    <Image className={styles["image-slider"]} src={visibleImage.data.image} alt={visibleImage.data.alt} />
+    <Image 
+      onAnimationIteration={changeVisibleImage}
+      className={styles["image-slider"]} 
+      alt={visibleImage.data.alt} 
+      src={visibleImage.data.image} 
+    />
   );
 }
