06bc6b107fb0b4b44a3905db9047f1abd22fe33b
Danielle Luz <daniln2209@gmail.com>
2025-04-21 06:44:50 -0300

    feat: create meals header structure

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,17 @@
+import Link from "next/link";
+import styles from "./styles.module.css";
+
 export default function Meals() {
   return (
     <main>
-      <h1>Meals</h1>
+      <article>
+        <h2 className={styles["header-title"]}>
+          Delicious meals, created 
+          <span className={styles["highlight"]}>by you</span>
+        </h2>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <Link className={styles["header-link"]} href="/meals/share">Share your favorite recipe</Link>
+      </article>
     </main>
   );
 }
