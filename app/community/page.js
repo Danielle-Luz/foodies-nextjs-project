39abadd4b8ffeb0d5a262527da892b5f6e8e96f0
Danielle Luz <daniln2209@gmail.com>
2025-04-21 06:20:13 -0300

    feat: styled community content

--- app/community/page.js
+++ app/community/page.js
@@ -4,19 +4,19 @@ import { perks } from './perks-data';
 
 export default function Community() {
   return (
-    <main>
+    <main className={`horizontal-spacing ${styles["content"]}`}>
       <article className={styles["header"]}>
         <h2 className={styles["header-title"]}>One shared passion: <span className={styles.highlight}>Food</span></h2>
         <p className={styles["header-text"]}>Join our community and share your favorite recipes!</p>
       </article>
-      <section className={styles["content"]}>
-        <h2 className={styles["perks-title"]}>Community Perks</h2>
+      <section className={styles["perks-content"]}>
+        <h2>Community Perks</h2>
         <ul className={styles["perks-list"]}>
           {
             perks.map(perk => {
               <li className={styles["perks-list-item"]}>
-                <Image className={styles["perks-image"]} src={perk.src} alt={perk.alt} />
                 <p className={styles["perks-content"]}>{perk.content}</p>
+                <Image className={styles["perks-image"]} src={perk.src} alt={perk.alt} />
               </li>
             })
           }
