8c805013c03b68799660ff7f444397af7cb2115b
Danielle Luz <daniln2209@gmail.com>
2025-04-21 06:51:05 -0300

    feat: style meals header

--- app/meals/page.js
+++ app/meals/page.js
@@ -3,11 +3,10 @@ import styles from "./styles.module.css";
 
 export default function Meals() {
   return (
-    <main>
-      <article>
+    <main className="horizontal-spacing">
+      <article className={styles["header"]}>
         <h2 className={styles["header-title"]}>
-          Delicious meals, created 
-          <span className={styles["highlight"]}>by you</span>
+          Delicious meals, created <span className={styles["highlight"]}>by you</span>
         </h2>
         <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
         <Link className={styles["header-link"]} href="/meals/share">Share your favorite recipe</Link>
