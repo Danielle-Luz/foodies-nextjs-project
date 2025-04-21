b0b2c8c8cb863285eac5dd172997aae3d70c4c3c
Danielle Luz <daniln2209@gmail.com>
2025-04-21 05:21:32 -0300

    feat: create perks data and add css classes

--- app/community/page.js
+++ app/community/page.js
@@ -1,12 +1,27 @@
 import Image from 'next/image';
-import mealIcon from '@/assets/icons/meal.png';
-import communityIcon from '@/assets/icons/community.png';
-import eventsIcon from '@/assets/icons/events.png';
+import styles from "./styles.module.css";
+import { perks } from './perks-data';
 
 export default function Community() {
   return (
     <main>
-      <h1>Community</h1>
+      <article className={styles["header"]}>
+        <h2 className={styles["header-title"]}>One shared passion: <span className={styles.highlight}>Food</span></h2>
+        <p className={styles["header-text"]}>Join our community and share your favorite recipes!</p>
+      </article>
+      <section className={styles["content"]}>
+        <h2 className={styles["perks-title"]}>Community Perks</h2>
+        <ul className={styles["perks-list"]}>
+          {
+            perks.map(perk => {
+              <li className={styles["perks-list-item"]}>
+                <Image className={styles["perks-image"]} src={perk.src} alt={perk.alt} />
+                <p className={styles["perks-content"]}>{perk.content}</p>
+              </li>
+            })
+          }
+        </ul>
+      </section>
     </main>
   );
 }
