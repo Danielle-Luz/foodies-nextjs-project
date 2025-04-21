e8cc7638b5f81ff04130551575ce4ac1a124230b
Danielle Luz <daniln2209@gmail.com>
2025-04-21 06:58:14 -0300

    feat: changed header text font size

--- app/meals/page.js
+++ app/meals/page.js
@@ -8,7 +8,7 @@ export default function Meals() {
         <h2 className={styles["header-title"]}>
           Delicious meals, created <span className={styles["highlight"]}>by you</span>
         </h2>
-        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={styles["header-text"]}>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
         <Link className={styles["header-link"]} href="/meals/share">Share your favorite recipe</Link>
       </article>
     </main>
