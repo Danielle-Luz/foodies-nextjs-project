b8d5404f3b5acc60f889d9b8e28d40a114ce421e
Danielle Luz <daniln2209@gmail.com>
2025-04-21 04:53:34 -0300

    feat: add path root

--- components/navbar-link/navbar-link.js
+++ components/navbar-link/navbar-link.js
@@ -1,6 +1,9 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavbarLink({href, children}) {
+  const currentPath = usePathname();
+
   return (
     <Link className={`${styles.navbarLink}`} href={href}>{children}</Link>
   );
