6522bcb7b438615c64cc51ca05be71f677406731
Danielle Luz <daniln2209@gmail.com>
2025-04-21 23:47:26 -0300

    feat: created error page

---
+++ app/error.js
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Error() {
+  return (
+    <article className="error">
+      <h1>An error ocurred!</h1>
+      <p>Failed to fetch meal data. Please try again.</p>
+    </article>
+  );
+}
