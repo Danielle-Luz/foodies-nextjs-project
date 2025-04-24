c8890da4415d27940410592241fe9fc40cab9d1a
Danielle Luz <daniln2209@gmail.com>
2025-04-23 22:01:18 -0300

    feat: created share page and started styling

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,7 +1,14 @@
+import styles from "./styles.module.css";
+
 export default function Share() {
   return (
-    <main>
-      <h1>Share</h1>
+    <main className={`horizontal-spacing ${styles["form-wrapper"]}`}>
+      <article>
+        <h1 className={styles["header-title"]}>Share your <span className={styles.highlight}>favorite meal</span></h1>
+        <p className={styles["header-description"]}>Or any other meal you feel needs sharing!</p>
+      </article>
+      <form>
+      </form>
     </main>
   );
 }
