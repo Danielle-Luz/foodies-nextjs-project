b559fef5370a72b6ff16db899ae451ea10497757
Danielle Luz <daniln2209@gmail.com>
2025-05-26 00:53:29 -0300

    feat: added slug creation and store image as file

--- api/meals/index.js
+++ api/meals/index.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const sql = require("better-sqlite3");
 const database = sql("meals.db");
 
@@ -9,13 +10,27 @@ export function getMeal(slug) {
   return database.prepare("SELECT * FROM meals WHERE slug = ?").get(slug);
 }
 
-export function createMeal(newMeal) {
+export async function createMeal(newMeal) {
   const whiteSpaceRegex = /\s/g;
   const nonLetterCharactersRegex = /[^a-zA-Z]/g;
 
   const titleWithoutSpaces = newMeal.title.toLowerCase().replace(whiteSpaceRegex, "-");
   const titleOnlyWithLetters = titleWithoutSpaces.replace(nonLetterCharactersRegex, "");
   newMeal.slug = titleOnlyWithLetters;
+
+  const imageFileName =` ${titleOnlyWithLetters}.${imageFormat}`;
+  const imageFormat = newMeal.image.name.split(".").pop();
+  const imageStream = fs.createWriteStream(`public/images/${imageFileName}`);
+  const bufferedImage = await newMeal.image.arrayBuffer();
+
+  imageStream.write(Buffer.from(bufferedImage), (error) =>  {
+      if(error) {
+        new Error("An error happened while saving the image");
+      }
+    }
+  );
+
+  newMeal.image = `images/${imageFileName}`;
 
   const insertStatement = database.prepare(`
     INSERT INTO meals  
