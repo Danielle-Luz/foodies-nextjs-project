7e183d51293b5e940a7b3ab295daad425a8c37b8
Danielle Luz <daniln2209@gmail.com>
2025-05-31 13:28:27 -0300

    feat: added close button

--- components/popup/index.js
+++ components/popup/index.js
@@ -4,8 +4,11 @@ export default function Popup({ title, text }) {
   return (
     <article className={styles["popup-background"]}>
       <div className={styles["popup-wrapper"]}>
-        <button>
-          x
+        <button className={styles["close-button"]}>
+          <img
+            src="https://img.icons8.com/material-rounded/100/ffffff/delete-sign.png"
+            alt="close popup icon"
+          />
         </button>
         <article className={styles["popup"]}>
           <h2>{title}</h2>
@@ -13,5 +16,5 @@ export default function Popup({ title, text }) {
         </article>
       </div>
     </article>
-  )
+  );
 }
