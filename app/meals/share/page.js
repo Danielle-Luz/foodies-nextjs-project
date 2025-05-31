969724eeda599f438b56717c1d35bfd063eb29ac
Danielle Luz <daniln2209@gmail.com>
2025-05-31 13:21:05 -0300

    feat: styled popup

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -6,31 +6,35 @@ import ImagePicker from "@/components/image-picker/image-picker";
 import { onSubmit } from "./handleForm";
 import FormButton from "@/components/form-button";
 import { useFormState } from "react-dom";
+import Popup from "@/components/popup";
 
 export default function Share() {
   const [state, formAction] = useFormState(onSubmit, {});
 
   return (
-    <main className={`horizontal-spacing ${styles["form-wrapper"]}`}>
-      <article>
-        <h1 className={styles["header-title"]}>
-          Share your <span className={styles.highlight}>favorite meal</span>
-        </h1>
-        <p className={styles["header-description"]}>
-          Or any other meal you feel needs sharing!
-        </p>
-      </article>
-      <form className={styles.form} action={formAction}>
-        <article className={styles["field-row"]}>
-          <Field label="Your name" name="creator" />
-          <Field label="Your email" name="creator_email" />
+    <>
+      <Popup title="title" text="text" />
+      <main className={`horizontal-spacing ${styles["form-wrapper"]}`}>
+        <article>
+          <h1 className={styles["header-title"]}>
+            Share your <span className={styles.highlight}>favorite meal</span>
+          </h1>
+          <p className={styles["header-description"]}>
+            Or any other meal you feel needs sharing!
+          </p>
         </article>
-        <Field label="Title" name="title" />
-        <Field label="Short summary" name="summary" />
-        <Field label="Instructions" name="instructions" type="textarea" />
-        <ImagePicker label="Pick a image" name="image" />
-        <FormButton buttonLabel="Share meal" />
-      </form>
-    </main>
+        <form className={styles.form} action={formAction}>
+          <article className={styles["field-row"]}>
+            <Field label="Your name" name="creator" />
+            <Field label="Your email" name="creator_email" />
+          </article>
+          <Field label="Title" name="title" />
+          <Field label="Short summary" name="summary" />
+          <Field label="Instructions" name="instructions" type="textarea" />
+          <ImagePicker label="Pick a image" name="image" />
+          <FormButton buttonLabel="Share meal" />
+        </form>
+      </main>
+    </>
   );
 }
