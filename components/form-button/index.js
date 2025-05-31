a22b777181a80deb35e547a39d9685d1b7ae6a67
Danielle Luz <daniln2209@gmail.com>
2025-05-31 06:43:22 -0300

    feat: add wait text while form is being submitted

--- components/form-button/index.js
+++ components/form-button/index.js
@@ -1,11 +1,17 @@
+'use client'
+
+import { useFormStatus } from "react-dom";
 import styles from "./styles.module.css";
 
 export default function FormButton({ buttonLabel }) {
+  const { pending } = useFormStatus();
+
   return (
     <input
       className={styles["submit-button"]}
       type="submit"
-      value={buttonLabel}
+      value={pending ? "Wait..." : buttonLabel}
+      disabled={pending}
     />
   );
 }
