6651d545805a9874acb2eccc4c3443edd700b586
Danielle Luz <daniln2209@gmail.com>
2025-05-31 07:19:50 -0300

    feat: add validation on submitted data and add custom return messages

--- app/meals/share/handleForm.js
+++ app/meals/share/handleForm.js
@@ -2,7 +2,17 @@
 
 import { createMeal } from "@/api/meals";
 
-export async function onSubmit(formData) {
+function hasEmptyValues(data) {
+  const validatedData = Object.keys(data).map(key => {
+    const validatedValue = data[key];
+    return !validatedValue || validatedValue.trim().length() == 0;
+  })
+  
+  const empty = true;
+  return validatedData.includes(empty);
+}
+
+export async function onSubmit(previousValue, formData) {
   let newMeal = {};
 
   formData.keys().forEach(fieldName => {
@@ -12,6 +22,26 @@ export async function onSubmit(formData) {
       newMeal[fieldName] = formData.get(fieldName);
     }
   });
-  
-  createMeal(newMeal);
+
+  const { image, ...mealTextFields } = newMeal; 
+  const hasInvalidTextValues = hasEmptyValues(mealTextFields);
+
+  if(hasInvalidTextValues || image.size() == 0) {
+     return {
+      status: 400,
+      message: "There are empty fields in the form, make sure to fill in all the fields."
+     }
+  } else {
+    createMeal(newMeal).then(() => {
+      return {
+        status: 201,
+        message: "Meal shared with success!"
+      };
+    }).catch((error) => {
+      return {
+        status: 400,
+        message: error.message
+      };
+    });
+  }
 }
