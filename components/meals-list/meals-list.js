72568e920fc9547fba38856347d0fee467a6584a
Danielle Luz <daniln2209@gmail.com>
2025-06-01 10:36:00 -0300

    feat: added blur background on meals list

--- components/meals-list/meals-list.js
+++ components/meals-list/meals-list.js
@@ -1,28 +1,38 @@
 import Image from "next/image";
 import styles from "./styles.module.css";
 import Link from "next/link";
+import placeholderImage from "@/assets/black-background.jpg";
 
 export default function MealsList({ meals }) {
   return (
     <ul className={styles["meal-list"]}>
-      {
-        meals.map(
-          (meal, index) => {
-          return (
-            <li className={styles.meal} key={index}>
-              <Image className={styles["meal-image"]} src={meal.image} alt={meal.title} fill />
-              <article className={styles["meal-details"]}>
-                <header className={styles["meal-header"]}>
-                  <h3 className={styles["meal-title"]}>{meal.title}</h3>
-                  <p className={styles["meal-creation"]}>By {meal.creator}</p>
-                </header>
-                <p className={styles["meal-summary"]}>{meal.summary}</p>
-                <Link className={styles["meal-link"]} href={`/meals/${meal.slug}`}>View details</Link>
-              </article>
-            </li>
-          )
-        })
-      }
+      {meals.map((meal, index) => {
+        return (
+          <li className={styles.meal} key={index}>
+            <Image
+                className={styles["meal-image"]}
+                src={meal.image}
+                alt={meal.title}
+                placeholder="blur"
+                blurDataURL={placeholderImage.src}
+                fill
+              />
+            <article className={styles["meal-details"]}>
+              <header className={styles["meal-header"]}>
+                <h3 className={styles["meal-title"]}>{meal.title}</h3>
+                <p className={styles["meal-creation"]}>By {meal.creator}</p>
+              </header>
+              <p className={styles["meal-summary"]}>{meal.summary}</p>
+              <Link
+                className={styles["meal-link"]}
+                href={`/meals/${meal.slug}`}
+              >
+                View details
+              </Link>
+            </article>
+          </li>
+        );
+      })}
     </ul>
   );
 }
